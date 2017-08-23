1f6a7458008c56bb1bb488f4ab11071df5378ab2
Kyle Patrick Caspers <k.caspers@comcast.net>
2017-08-23 09:27:35 -0500

    sending rest

---
+++ html/PortfolioSite/scriptforrestuarant.js
@@ -0,0 +1,6 @@
+ function greetTheUser() {
+          document.getElementById("userName").innerText =
+            document.getElementById("inputName").value;
+          document.getElementById("greeting").style.display = 
+            "block";
+      }
