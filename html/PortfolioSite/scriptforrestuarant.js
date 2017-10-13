6fe23037f16fad784c235d0ccdfaae0dd63a713c
Kyle Patrick Caspers <k.caspers@comcast.net>
2017-10-12 22:48:20 -0500

    got rid of some gross stuff

--- html/PortfolioSite/scriptforrestuarant.js
+++
@@ -1,6 +0,0 @@
- function greetTheUser() {
-          document.getElementById("userName").innerText =
-            document.getElementById("inputName").value;
-          document.getElementById("greeting").style.display = 
-            "block";
-      }
