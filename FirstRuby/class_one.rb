919ae0bc9189c98db53575ccf624953f39d3294a
Kyle Patrick Caspers <k.caspers@comcast.net>
2017-11-26 16:36:21 -0600

    renaming pesky class

--- FirstRuby/class_one.rb
+++ FirstRuby/class_one.rb
@@ -1,12 +1,13 @@
 class ClassOne
-
-  puts 'I will calculate the factorial of a number you give me.'
-  userInput = Integer(gets.chomp)
-  index = 0
-  factorial = 1
-  while index < userInput do
-    factorial *= index+1
-    index = index + 1
+  def factorialCalc
+    puts 'I will calculate the factorial of a number you give me.'
+    userInput = Integer(gets.chomp)
+    index = 0
+    factorial = 1
+    while index < userInput do
+      factorial *= index+1
+      index = index + 1
+    end
+    puts factorial
   end
-  puts factorial
 end
---
+++ FirstRuby/main.rb
@@ -0,0 +1,4 @@
+class Main
+factorialCalc = ClassOne.new
+
+end
