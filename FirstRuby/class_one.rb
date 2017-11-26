27556dce4a18bf13b21811320c79fedadef6d2b1
Kyle Patrick Caspers <k.caspers@comcast.net>
2017-11-26 16:14:12 -0600

    Starting to play with Ruby, not much to see here yet

---
+++ FirstRuby/class_one.rb
@@ -0,0 +1,12 @@
+class ClassOne
+
+  puts 'I will calculate the factorial of a number you give me.'
+  userInput = Integer(gets.chomp)
+  index = 0
+  factorial = 1
+  while index < userInput do
+    factorial *= index+1
+    index = index + 1
+  end
+  puts factorial
+end
